9e125507cf4cd647859fd06789ca95dba0b1a021
Eliza Munroe <era3munroe@gmail.com>
2019-07-02 20:47:48 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,11 +3,14 @@ def second_challenge
     :montague => {
       :patriarch => {}
       :matriarch => {}
-      
-      }, 
-    :capulet => {
       :hero => {}
       :hero_friends => []
+      }, 
+    :capulet => {
+      :patriarch => {}
+      :matriarch => {}
+      :heroine => {}
+      :heroine_friends => []
       }
     
   }
