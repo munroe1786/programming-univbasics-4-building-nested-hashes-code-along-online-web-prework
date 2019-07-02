235e74ab104c8620ef408e2e11c713f86c6fd5b0
Eliza Munroe <era3munroe@gmail.com>
2019-07-02 21:27:53 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,12 @@ def bonus
    }
   }
 
-  #code your solution here:
+  
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  
+  puts epic_tragedy
 
   
 
